0c03f9c5ec9d44d0677406d3400ae5ae2060b506
mrlinh9988 <mrlinhdeptrai9988@gmail.com>
2020-04-18 22:56:05 +0700

    Updates

---
+++ precache-manifest.5b032457db1f092c207b0f4f6c6f97ee.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84d1830ce705d17c83852e09ac3b7e3f",
+    "url": "/robo_friends_app/index.html"
+  },
+  {
+    "revision": "6fb695216b068950ce8a",
+    "url": "/robo_friends_app/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "619a3ef32541cf08a0c6",
+    "url": "/robo_friends_app/static/css/main.3f7f6141.chunk.css"
+  },
+  {
+    "revision": "6fb695216b068950ce8a",
+    "url": "/robo_friends_app/static/js/2.0296bd1f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robo_friends_app/static/js/2.0296bd1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "619a3ef32541cf08a0c6",
+    "url": "/robo_friends_app/static/js/main.93255863.chunk.js"
+  },
+  {
+    "revision": "13bea644b42b7853cf13",
+    "url": "/robo_friends_app/static/js/runtime-main.3a5140f6.js"
+  },
+  {
+    "revision": "603a56d0d52fe6bbfd13b08a1195b7e8",
+    "url": "/robo_friends_app/static/media/Chopsic-K6Dp.603a56d0.ttf"
+  }
+]);
