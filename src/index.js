f98b4103680ee73f6cda5cd0befd5fe0706aef7f
mrlinh <mrlinhdeptrai9988@gmail.com>
2020-05-16 00:51:31 +0700

    change config.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunkMiddleware from 'redux-thunk';
@@ -26,22 +25,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 registerServiceWorker();
-=======
-import './index.css';
-
-import App from './containers/App';
-import * as serviceWorker from './serviceWorker';
-import 'tachyons';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
->>>>>>> 18e510b2fbcbff1bea9c066f7ad9f2ff9cf41815
